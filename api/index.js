bdae614b10e978867982655881516a395dc5e7ec
zendi014 <48636084+zendi014@users.noreply.github.com>
2021-11-26 11:30:36 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,8 +24,7 @@ bot.onText(/\/show_url/, (msg) => {
     bot.sendMessage(
         global_msg_id,
         `
-            https://esp-telebot.herokuapp.com/api/sensor/123/65/78 \n
-            https://esp-telebot.herokuapp.com/api/test/cobacoba
+            Contoh URL::/nhttps://esp-telebot.herokuapp.com/api/sensor/123/65/78 \nhttps://esp-telebot.herokuapp.com/api/test/cobacoba
         `
     );
 });
